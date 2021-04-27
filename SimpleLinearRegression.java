fb62290c8f096c7be5d65c544d7bf60c8aa37c37
unknown <julio.gonzalez7589@alumnos.udg.mx>
2021-04-27 16:12:24 -0500

    Set up SLR class

---
+++ SimpleLinearRegression.java
@@ -0,0 +1,21 @@
+public class SimpleLinearRegression {
+	// private float b0;
+	// private float b1;
+
+
+	public static void main(String args[]) {
+		float dataSetX[] = {23, 26, 30, 34, 43, 48, 52, 57, 58};
+		float dataSetY[] = {
+			651,
+			762,
+			856,
+			1063,
+			1190,
+			1298,
+			1421,
+			1440,
+			1518
+		};
+		System.out.println(dataSetX[0]);
+	}
+}
